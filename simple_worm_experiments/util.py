af83de964501ef54296d7448121d0309ea87223e
Lukas <scld@leeds.ac.uk>
2022-08-17 19:35:35 +0100

    Added more functionality and fixed some bugs

--- simple_worm_experiments/util.py
+++ simple_worm_experiments/util.py
@@ -8,16 +8,21 @@ Created on 28 Jun 2022
 # Build-in importsO
 import itertools as it
 from multiprocessing import Pool
+from os.path import isfile
+import traceback
+import json
+import numpy as np
 
 # Third party imports
-import numpy as np
-from matplotlib.cm import get_cmap
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
 import pickle
 from parameter_scan.util import dict_hash
 
 # Local imports
 from simple_worm.model_parameters import PhysicalParameters, ModelParameters, PP_KEYS, MP_KEYS, MODP_KEYS
-from simple_worm.rod.solver import Solver
+from simple_worm.rod.solver import Solver, SOLVER_KEYS
+
 
 #------------------------------------------------------------------------------ 
 # Simulation 
@@ -56,6 +61,31 @@ def load_data(prefix, data_path, parameter, encoding = 'rb'):
     data = pickle.load(open(data_path + filename + '.dat', encoding))
     
     return data
+
+def save_output(data_path, fn, FS, MP, CS, parameter, save = 'min'):
+
+    output = {}
+
+    output['parameter'] = parameter
+    output['MP'] = MP
+        
+    if save == 'all':    
+        
+        output['FS'] = FS        
+        output['CS'] = CS.to_numpy()
+    
+    elif save == 'min':
+                
+        output['x'] = FS.x
+        output['x_t'] = FS.x_t
+        output['k'] = FS.Omega
+        output['k0'] = CS.Omega
+        output['t'] = FS.times
+        output['pic'] = FS.pic
+                                                            
+    pickle.dump(output, open(data_path + '/simulations/' + fn + '.dat', 'wb'))
+       
+       
        
 #===============================================================================
 # Post processing
@@ -76,38 +106,41 @@ def align_FS_arr(FS_arr, N_arr, dt_arr):
         
     return aligned_FS_arr, N_min, dt_max     
 
-def frame_trafo(mat, FS, trafo = 'l2b'):
+def frame_trafo(v_mat, FS, trafo = 'l2b'):
     
     e1_mat = FS.e1
     e2_mat = FS.e2
     e3_mat = FS.e3
                 
-    mat_bar = np.zeros_like(mat)
+    v_mat_bar = np.zeros_like(v_mat)
                                                   
-    for i, (arr, e1_arr, e2_arr, e3_arr) in enumerate(zip(mat, e1_mat, e2_mat, e3_mat)):
-        for j, (x, e1, e2, e3) in enumerate(zip(arr.T, e1_arr.T, e2_arr.T, e3_arr.T)):
+    for i, (arr, e1_arr, e2_arr, e3_arr) in enumerate(zip(v_mat, e1_mat, e2_mat, e3_mat)):
+        for j, (v, e1, e2, e3) in enumerate(zip(arr.T, e1_arr.T, e2_arr.T, e3_arr.T)):
             
-            Q = np.vstack((e1, e2, e3)).T
+            Q = np.vstack((e1, e2, e3))
             
             # Transposed of Q transforms from lab to body frame
             if trafo == 'l2b':
-                Q = Q.T
-        
-            mat_bar[i, :, j] = np.matmul(Q, x)
-    
-    return mat_bar
+                v_mat_bar[i, :, j] = np.matmul(Q, v)
+            elif trafo == 'b2l':
+                v_mat_bar[i, :, j] = np.matmul(Q.T, v)
+                    
+    return v_mat_bar
         
 #===============================================================================
 # Simulation parameter
 
-def default_solver():
+def get_solver(parameter):
 
-    solver = Solver(linearization_method = 'picard_iteration', 
-                    finite_difference_order={1:2, 2:2},
-                    deepcopy = True)
+    solver_param = {k: parameter[k] for k in parameter if k in SOLVER_KEYS}
+
+    return Solver(**solver_param)
+
+def default_solver():
     
-    return solver
-            
+    return Solver()
+
+           
 def default_parameter(parameter = None):
     
     # Simulation parameter
@@ -118,7 +151,8 @@ def default_parameter(parameter = None):
     external_force = ['sbt']
     rft = 'Whang'
     use_inertia = False
-        
+    rc = 'spheroid'
+    
     # Geometric parameter    
     L0 = 1130 * 1e-6
     r_max = 32 * 1e-6
@@ -138,7 +172,8 @@ def default_parameter(parameter = None):
     parameter['external_force'] = external_force
     parameter['use_inertia'] = use_inertia
     parameter['rft'] = rft
-    
+    parameter['rc'] = rc
+        
     parameter['N']  = N
     parameter['dt'] = dt
     parameter['L0'] = L0
@@ -169,6 +204,8 @@ def dimensionless_MP(parameter):
         MP.c_n = MP_param['c_n']
                    
     return MP
+
+
 
 #===============================================================================
 # Analyse simulation results 
@@ -251,9 +288,10 @@ def comp_midline_error(FS_arr, FS_0):
 #------------------------------------------------------------------------------ 
 # Plotting
     
-def color_list_from_values(val_arr, cmap = None, log = False):
-    if cmap is None:        
-        cmap = get_cmap('plasma')
+def color_list_from_values(val_arr, map_name = None, log = False):
+
+    if map_name is None:    
+        cmap = cm.get_cmap('plasma')    
     
     # use log-scale if all values are positive
     if log:
@@ -269,7 +307,23 @@ def color_list_from_values(val_arr, cmap = None, log = False):
     
     return color_list
 
+def color_map_from_values(c_arr, log = True, map_name = None):
+    
+    if map_name is None:    
+        cmap = cm.get_cmap('plasma')
 
+    if log:
+        assert np.all(c_arr > 0)        
+        c_arr = np.log10(c_arr)
+            
+    normalize = mcolors.Normalize(vmin = np.min(c_arr), vmax = np.max(c_arr))
+    s_map = cm.ScalarMappable(norm=normalize, cmap = cmap)
+  
+    c_list = s_map.to_rgba(c_arr)
+  
+    return s_map, cmap, c_list 
+  
+    
     
 
 
